65dc6874d54b0634bf81cb9ea6896fb3a65fb8ab
Алексей <alex-804@yandex.ru>
2024-08-20 18:17:04 +0500

    20/08/24

---
+++ module_2/module_2_3.py
@@ -0,0 +1,14 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+count = 0
+
+i = len(my_list)
+
+while True:
+    if my_list[count] > 0:
+        print(my_list[count])
+        count += 1
+    elif my_list[count] == 0:
+        count += 1
+        continue
+    else:
+        break
