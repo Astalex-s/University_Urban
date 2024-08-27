23056c6ac5299a9130b4dffecf7d8ad1deef94fe
Алексей <alex-804@yandex.ru>
2024-08-27 19:11:05 +0500

    26/08/24 unpacking positional parameters

---
+++ module_3/module_3_3.py
@@ -0,0 +1,20 @@
+
+
+def print_params(a=1, b="strings", c=True):
+    print(a, b, c)
+
+
+print_params(b=25)
+print_params(c=[1,2,3])
+
+
+values_list = [2, 'string', True]
+values_dict = {'a': 5, 'b': True, 'c': 'string'}
+
+
+print_params(*values_list)
+print_params(**values_dict)
+
+
+values_list_2 = [3.3, False]
+print_params(*values_list_2, 42)
