bb452e8414174ea123197e4c11a61f4d39f1f417
Алексей <alex-804@yandex.ru>
2024-09-29 21:09:04 +0500

    29/09/24 Homework on the topic "Why do we need inheritance". The task "Edible, inedible"

---
+++ module_6/module_6_1.py
@@ -0,0 +1,65 @@
+class Animal:     # класс животных
+    alive = True  # живой
+    fed = False   # накормленый
+
+    def __init__(self, name):
+        self.name = name  # индивидуальное название каждого животного
+
+
+class Plant:        # класс растений
+    edible = False  # съедобность
+
+    def __init__(self, name):
+        self.name = name  # индивидуальное название каждого растения
+
+
+class Mammal(Animal):  # Млекопитающие
+
+    def eat(self, food):  # food - это параметр, принимающий объекты классов растений
+        if food.edible is True:
+            print(f'{self.name} съел {food.name}')
+            self.fed = True
+        else:
+            print(f'{self.name} не стал есть {food.name}')
+            self.alive = False
+
+
+class Predator(Animal):  # Хищник
+    """
+    Метод eat должен работать следующим образом:
+    Если переданное растение (food) съедобное - выводит на экран "<self.name> съел <food.name>", меняется атрибут
+    fed на True.
+    Если переданное растение (food) не съедобное - выводит на экран "<self.name> не стал есть <food.name>",
+    меняется атрибут alive на False.
+    Т.е если животному дать съедобное растение, то животное насытится, если не съедобное - погибнет.
+    """
+    def eat(self, food):
+        if food.edible is True:
+            print(f'{self.name} съел {food.name}')
+            self.fed = True     # переопределение атрибута при наследовании
+        else:
+            print(f'{self.name} не стал есть {food.name}')
+            self.alive = False  # переопределение атрибута при наследовании
+
+
+class Flower(Plant):  # класс цветов
+    pass
+
+
+class Fruit(Plant):  # класс фруктов
+    edible = True    # переопределение атрибута при наследовании
+
+
+a1 = Predator('Волк с Уолл-Стрит')
+a2 = Mammal('Хатико')
+
+p1 = Flower('Цветик семицветик')
+p2 = Fruit('Заводной апельсин')
+print(a1.name)
+print(p1.name)
+print(a1.alive)
+print(a2.fed)
+a1.eat(p1)
+a2.eat(p2)
+print(a1.alive)
+print(a2.fed)
