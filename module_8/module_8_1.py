e19b25845c740d4594134aa1a2dcec3b435be887
Алексей <alex-804@yandex.ru>
2024-10-15 16:12:55 +0500

    15/10/24 Homework for the lesson "Try and Except". Task "Programmers can do anything"

---
+++ module_8/module_8_1.py
@@ -0,0 +1,12 @@
+def add_everything_up(a, b):
+    try:
+        return round(a + b, 3)
+    except TypeError:
+        if isinstance(a, str) and isinstance(b, str):
+            return a + b
+        return str(a) + str(b)
+
+
+print(add_everything_up(123.456, 'строка'))
+print(add_everything_up('яблоко', 4215))
+print(add_everything_up(123.456, 7))
