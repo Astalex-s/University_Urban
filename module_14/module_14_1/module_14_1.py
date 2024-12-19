9ed1f8a7726b77fdee71c1eb53b7cdc2334c5068
Алексей <alex-804@yandex.ru>
2024-12-19 21:57:12 +0500

    19/12/24 Homework on the topic "Creating a database, adding, selecting and deleting elements."

--- module_14/module_14_1/module_14_1.py
+++ module_14/module_14_1/module_14_1.py
@@ -14,17 +14,17 @@ balance INTEGER NOT NULL
 ''')
 # ЗАПОЛНЯЕМ БАЗУ ДАННЫХ 10-Ю НОВЫМИ ЗАПИСЯМИ:
 
-# for i in range(1, 11):
-#     cursor.execute("INSERT INTO Users(username, email, age, balance) VALUES (?, ?, ?, ?)",
-#                    (f"User{i}", f"example{i}@gmail.com", f"{i * 10}", 1000))
+for i in range(1, 11):
+    cursor.execute("INSERT INTO Users(username, email, age, balance) VALUES (?, ?, ?, ?)",
+                   (f"User{i}", f"example{i}@gmail.com", f"{i * 10}", 1000))
 
 # ОБНОВЛЯЕМ balance У КАЖДОЙ 2-ОЙ ЗАПИСИ НАЧИНАЯ С 1-ОЙ НА 500:
-# for i in range(1, 11, 2):
-#     cursor.execute("UPDATE Users SET balance = ? WHERE id = ?", (500, f"{i}"))
+for i in range(1, 11, 2):
+    cursor.execute("UPDATE Users SET balance = ? WHERE id = ?", (500, i))
 
 # УДАЛЯЕМ КАЖДУЮ 3-Ю ЗАПИСЬ В ТАБЛИЦК НАЧИНАЯ С 1-Й:
-# for i in range(1, 11, 3):
-#     cursor.execute("DELETE FROM Users WHERE id = ?", (f"{i}",))
+for i in range(1, 11, 3):
+    cursor.execute("DELETE FROM Users WHERE id = ?", (i,))
 
 # ДЕЛАЕМ ВЫБОРКУ ВСЕХ ЗАПИСЕЙ, КРОМЕ ЗАПИСИ ГДЕ ВОЗРАСТ РАВЕН 60:
 cursor.execute("SELECT username, email, age, balance FROM Users WHERE age != ?", (60,))
